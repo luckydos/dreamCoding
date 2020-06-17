eef9191e52b9b27f214d856dedbbc38fdf8b2943
unknown <luckydos@gmail.com>
2020-06-17 14:11:33 +0900

    edit

--- operator.js
+++ operator.js
@@ -76,3 +76,109 @@ console.log(`stringFive != numberFive : ${stringFive != numberFive}`);
 // == strict equality, no type conversion
 console.log(`stringFive === numberFive : ${stringFive === numberFive}`);
 console.log(`stringFive !== numberFive : ${stringFive !== numberFive}`);
+
+// object equality by reference
+const ellie1 = { name: 'ellie' };
+const ellie2 = { name: 'ellie' };
+const ellie3 = ellie1;
+console.log('ellie1 == ellie2 --> ' + (ellie1 == ellie2));
+console.log('ellie1 === ellie2 --> ' + (ellie1 === ellie2));
+console.log('ellie1 === ellie3 --> ' + (ellie1 === ellie3));
+
+// equality - puzzler
+console.log(0 == false); //true
+console.log(0 === false); //false
+console.log('' == false); //true
+console.log('' === false); //false
+console.log(null == undefined); //true
+console.log(null === undefined); //false
+console.log(`null type is ${typeof null}, undefined typs is ${undefined}`);
+
+// 8. Conditional operators: if
+// if, else if, else
+const name = 'aaa';
+if(name === 'aaa') {
+	console.log('You are name is aaa');
+} else if(name === 'bbb') {
+	console.log('You are name is bbb');
+} else {
+	console.log('Unkwnon');
+}
+
+// 9. Ternary operator: ?
+// condition ? value1 : value2;
+console.log(name === 'aaa' ? 'print aaaa' : 'noooooooooooooooo');
+
+// 10. Switch statement
+// use for multiple if checks
+// use for enum-like value check
+// use for multiple type checks in TS
+const browser = 'Firefox';
+switch(browser) {
+	case 'IE':
+		console.log('go away!');
+		break;
+	case 'Chrome':
+		// console.log('love you!');
+		// break;
+	case 'Firefox':
+		console.log('ff bbb');
+		break;
+	default:
+		console.log('default................');
+		break;
+}
+
+// 11. Loops
+// while loop, while the condition is truthy,
+// body code is exceted.
+let i = 3;
+while(i > 0) {
+	console.log(`while: ${i}`);
+	i--;
+}
+
+// do while loop, body code is excuted first,
+// then check the condition.
+do {
+	console.log(`do while: ${i}`);
+	i--;
+} while(i > 0);
+
+// for loop, for(begin; condition; step)
+for(i = 3; i > 0; i--) {
+	console.log(`for: ${i}`);
+}
+
+for(let i = 3; i > 0; i = i - 2) {
+	console.log(`inline variable for: ${i}`);
+}
+
+// nested loops
+for(let a = 0; a < 3; a++) {
+	for(let b = 0; b < 5; b++) {
+		console.log(`a: ${a}, b: ${b}`);
+	}
+}
+
+// break, continue
+// Q1. iterate from 0 to 10 and print only even numbers (use continue)
+for(let n = 0; n < 11; n++) {
+	if(n % 2 !== 0) {
+		continue;
+	}
+	console.log(`Q1: ${n}`);
+
+	// not use continue
+	// if(n % 2 === 0) {
+	// 	console.log(`Q1: ${n}`);
+	// }
+}
+
+// Q2. iterate fro m0 to 10 and print number until reaching 8 (use break)
+for (let n = 0; n < 11; n ++) {
+	if(n > 8) {
+		break;
+	}
+	console.log(`Q2: ${n}`);
+}
