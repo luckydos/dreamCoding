f947aa402f0a74dd596d2dad6e0de17cc846a9e8
unknown <luckydos@gmail.com>
2020-06-19 13:27:06 +0900

    edit

---
+++ class.js
@@ -0,0 +1,28 @@
+'use strict';
+
+// Object-oriendted programming
+// class: template
+// object: instance of a class
+// JavaScript classes
+//  - introduced in ES6
+//  - syntactical sugar over prototype-based inheritance
+
+// 1. Class declarations
+class Person {
+	// constructor
+	constructor(name, age) {
+		// fields
+		this.name = name;
+		this.age = age;
+	}
+
+	// methods
+	speak() {
+		console.log(`${this.name}: hello!`);
+	}
+}
+
+const ellie = new Person('ellie', 30);
+console.log(ellie.name);
+console.log(ellie.age);
+ellie.speak();
